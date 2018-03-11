9db5cd00c33868c10e4ec368f3a02ddeefdb6803
ichbestimmtnicht <ichbestimmtnicht@users.noreply.github.com>
2018-03-11 21:44:09 +0100

    Serrrrrrrrrrconfig

--- config/dietpi/post-server.sh
+++ config/dietpi/post-server.sh
@@ -7,8 +7,6 @@ git config --global user.name "ichbestimmtnicht"
 sudo apt install zsh -y
 #Change Defaul Shell to Zsh
 sudo chsh -s /usr/bin/zsh
-#ohmyzsh Commandline Tool
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #Prequisite for apt-key adv
 sudo apt install dirmngr -y
 #Debian Stretch Backport Keys
@@ -18,3 +16,5 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
 echo 'deb http://httpredir.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/stretch-backports.list
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove -y
+#ohmyzsh Commandline Tool
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
