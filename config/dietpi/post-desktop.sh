1f179c5bfe9e5be3cb92491de0194fa9edad89c5
ichbestimmtnicht <ichbestimmtnicht@users.noreply.github.com>
2018-03-13 01:56:22 +0100

    zsh config test

--- config/dietpi/post-desktop.sh
+++ config/dietpi/post-desktop.sh
@@ -1,4 +1,17 @@
 #!bin/bash
+
+#Update
+#Prequisite for apt-key adv
+sudo apt install dirmngr -y
+#Debian Stretch Backport Keys
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7638D0442B90D010
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
+#Debian backports Stretch
+echo 'deb http://httpredir.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/stretch-backpor$
+sudo apt update && sudo apt upgrade -y
+#Autoremove
+sudo apt autoremove -y && sudo apt autolean -y && sudo apt clean -y
+
 #Git
 sudo apt install git -y
 git config --global user.email "ichbestimmtnicht@users.noreply.github.com"
@@ -12,25 +25,16 @@ sudo apt install alacarte -y
 #Guake Terminal Panel
 sudo apt install guake -y
 sudo apt install guake-indicator -y
+#Telegram Messenger
+sudo apt install telegram-desktop -y
+#Dont ask for anythin
+DEBIAN_FRONTEND=noninteractive sudo apt install -q -y rpi-chromium-mods
+
 #An other shell
 sudo apt install zsh -y
 #Change Defaul Shell to Zsh
 sudo chsh -s /usr/bin/zsh
 #ohmyzsh Commandline Tool
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#Prequisite for apt-key adv
-sudo apt install dirmngr -y
-#Debian Stretch Backport Keys
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7638D0442B90D010
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
-#Debian backports Stretch
-echo 'deb http://httpredir.debian.org/debian stretch-backports main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/stretch-backports.list
-sudo apt update
-#Telegram Messenger
-sudo apt install telegram-desktop -y
-#Dont ask for anythin
-export DEBIAN_FRONTEND=noninteractive
-#Better Youtube
-sudo apt install -q -y rpi-chromium-mods
-#Autoremove
-sudo apt autoremove -y && sudo apt autolean -y
+#Get zsh configuration
+curl -o /home/dietpi/.zshrc https://raw.githubusercontent.com/ichbestimmtnicht/automatic-memory/master/config/zsh/zshrc-desktop.md
